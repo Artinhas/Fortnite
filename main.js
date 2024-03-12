74a318fc1f2bfc762fe2d354dfa77e4d2bfe5a09
Artinhas <162718676+Artinhas@users.noreply.github.com>
2024-03-12 11:24:26 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://pbs.twimg.com/profile_images/1731222803267952641/7kYg5SWA_400x400.jpg";
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://cdn1.epicgames.com/offer/fn/28BR_C5S1_EGS_Launcher_Blade_2560x1440_2560x1440-e598d8d28d63a6deafe2b14a2e42cd01?h=270&quality=medium&resize=1&w=480"
+}
